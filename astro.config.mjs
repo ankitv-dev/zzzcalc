9b98e843ed7e46c6d9af1566b8ca61bb3ba0b57a
ankitv-dev <ankit.kumar.verma.dev@gmail.com>
2026-06-18 21:16:36 +0530

    add sitemap filter, disallow error pages in robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://zzzcalc.vercel.app',
   integrations: [sitemap({
+    filter: (page) => !page.includes('/404') && !page.includes('/500'),
     namespaces: {
       news: false,
       xhtml: false,
